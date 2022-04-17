bc3ee0a9e88a99f80abaeb759e49a1071f95fe0f
mpuryear <mathewpuryear@gmail.com>
2022-04-17 14:43:53 -0700

    Update npc text option string

--- src/70skip.cpp
+++ src/70skip.cpp
@@ -33,8 +33,7 @@ public:
       switch(gossipListId)
       {
       case 11:
-         AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "Yes", GOSSIP_SENDER_MAIN, 12);
-	 AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "No", GOSSIP_SENDER_MAIN, 13);
+         AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, LOCALE_SKIP_0, GOSSIP_SENDER_MAIN, 12);
          SendGossipMenuFor(player, player->GetGossipTextId(creature), creature->GetGUID());
          break;
 
@@ -47,10 +46,6 @@ public:
          CloseGossipMenuFor(player);
          break;
       
-      case 13: 
-         CloseGossipMenuFor(player);
-         break;
-  
       default:
          break;
       }
