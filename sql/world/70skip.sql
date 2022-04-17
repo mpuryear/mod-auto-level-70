4a5991b6be98279f6dfdc1e9eb4544597f171df2
mpuryear <mathewpuryear@gmail.com>
2022-04-17 13:56:59 -0700

    fixup! Add custom NPC to boost to 70

--- sql/world/70skip.sql
+++ sql/world/70skip.sql
@@ -5,22 +5,22 @@ SET
 @Title := "Seventy Booster",
 @Icon := "Speak",
 @GossipMenu := 0,
-@MinLevel := 1,
+@MinLevel := 80,
 @MaxLevel := 80,
 @Faction := 35,
-@NPCFlag := 80,
+@NPCFlag := 1,
 @Scale := 1.5,
 @Rank := 0,
 @Type := 7,
 @TypeFlags := 0,
-@FlagsExtra := 2,
+@FlagsExtra := 0,
 @AIName		:= "",
 @Script 	:= "npc_ac_70_skip";
 
 -- NPC
 DELETE FROM creature_template WHERE entry = @Entry;
-INSERT INTO creature_template (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `AiName`, `ScriptName`) VALUES
-(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, @Scale, @Rank, 1, 2, @Type, @TypeFlags, 1, @FlagsExtra, @AIName, @Script);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(@Entry, 0, 0, 0, 0, 0, @Model, 0, 0, 0, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, 0, @Faction, @NPCFlag, 1, 1.14286, 0.25, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, @AIName, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, @Script, 0);
 
 -- NPC EQUIPPED
 DELETE FROM `creature_equip_template` WHERE `CreatureID`=@Entry AND `ID`=1;
@@ -30,3 +30,4 @@ INSERT INTO `creature_equip_template` VALUES (@Entry, 1, 30095, 28825, 0, 0); --
 DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (@Entry);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
 (@Entry, 1, 1, 0, 0, 0, 0, NULL);
+
